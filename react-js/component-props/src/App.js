d96843110a65f0bd20ab765787b3393ffe091210
eko <ekomuliyo.com>
2020-01-30 08:09:40 +0700

    komponen dan props

--- react-js/component-props/src/App.js
+++ react-js/component-props/src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// komponen dan properti
+
+function Biodata(props){
+  return <span>{props.umur}</span>
+}
+
+function Sapaan(props){
+  return <h1>Haii {props.nama}, umur <Biodata umur={props.umur}/></h1>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Sapaan nama="Andi" umur="21"/>
+        <Sapaan nama="Budi" umur="22"/>
+        <Sapaan nama="Aan" umur="23"/>
+
       </header>
     </div>
   );
