09a0e80658152b538091621cf6f676d1bdcd85d7
eko <ekomuliyo.com>
2020-02-12 11:43:54 +0700

    latihan filter, map, reduce

--- higher-oder-function/index.js
+++ higher-oder-function/index.js
@@ -24,3 +24,42 @@ const result = array.filter(a => a > 5)
                     .map(a => a * 3)
                     .reduce((acc, cur) => acc + cur)
 console.log(result);
+
+
+// latihan filter, map, reduce
+
+// ambil semua elemen video
+const videos = Array.from(document.querySelectorAll('[data-duration]'))
+
+// pilih yang hanya 'Javascript Lanjutan'
+let result = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
+
+    // ambil durasi masing-masing video
+    .map(item => item.dataset.duration)
+
+    // ubah durasi menjadi int, ubah menit jadi detik
+    .map(waktu => {
+        // 10:30 = [10, 30]
+        const parts = waktu.split(':').map(part => parseInt(part))
+        return (parts[0] * 60) + parts[1]
+    })
+    
+    // jumlahkan semua detik
+    .reduce((acc, curr) => acc + curr)
+
+
+// ubah formatnya jadi jam menit detik
+const jam = Math.floor(result / 3600)
+result = result - jam * 3600
+const menit = Math.floor(result / 60);
+const detik = result - menit *60
+
+// simpan ke DOM
+const pDurasi = document.querySelector('.total-durasi')
+pDurasi.textContent = `${jam} Jam, ${menit} Menit, ${detik} Detik.`
+
+const jumlahVideo = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length
+const pJumlahVideo = document.querySelector('.jumlah-video')
+pJumlahVideo.textContent = `${jumlahVideo} Video.`
+
+console.log(detik);
