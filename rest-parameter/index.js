0b8f6512645f40cd8b9f77e81bdf84b859353942
eko muliyo <ekomuliyo@gmail.com>
2020-03-26 21:15:03 +0700

    rest-paramenter

--- rest-parameter/index.js
+++ rest-parameter/index.js
@@ -1,40 +1,40 @@
 // Rest Paramter
 // mengambil dari beberapa nilai yang di diambil dari argument sebuah function menjadi array
 
-// function myFunc(){
-//     // return `a = ${a}, b = ${b}, args = ${args}`;
-//     // return Array.from(arguments);
-//     return [...arguments];
-// }
+function myFunc(){
+    // return `a = ${a}, b = ${b}, args = ${args}`;
+    // return Array.from(arguments);
+    return [...arguments];
+}
 
-// console.log(myFunc(1, 2, 3, 4, 5));
+console.log(myFunc(1, 2, 3, 4, 5));
 
-// function jumlahkan(...angka){
-//     // let total = 0;
-//     // for(const a of angka){
-//     //     total += a;
-//     // }
-//     // return total;
-//     return angka.reduce((a, b) => a+b);
-// }
+function jumlahkan(...angka){
+    // let total = 0;
+    // for(const a of angka){
+    //     total += a;
+    // }
+    // return total;
+    return angka.reduce((a, b) => a+b);
+}
 
-// console.log(jumlahkan(1, 2, 4, 5, 6));
+console.log(jumlahkan(1, 2, 4, 5, 6));
 
-// // array destructuring
-// const kelompok1 = ['Eko', 'Andi', 'Budi'];
-// const [ketua, ...anggota] = kelompok1;
-// console.log(anggota);
+// array destructuring
+const kelompok1 = ['Eko', 'Andi', 'Budi'];
+const [ketua, ...anggota] = kelompok1;
+console.log(anggota);
 
-// // object desctructuring
-// const team = {
-//     pm: 'Eko Muliyo',
-//     frontEnd1: 'Budi',
-//     frontEnd2: 'Aan',
-//     backEnd: 'Arlan'
-// }
+// object desctructuring
+const team = {
+    pm: 'Eko Muliyo',
+    frontEnd1: 'Budi',
+    frontEnd2: 'Aan',
+    backEnd: 'Arlan'
+}
 
-// const {pm, ...myTeam} = team;
-// console.log(pm);
+const {pm, ...myTeam} = team;
+console.log(pm);
 
 // filtering
 function filterBy(type, ...values){
