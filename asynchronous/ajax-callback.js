c543644d7eca6fadc3317432f49364466e46977d
eko muliyo <ekomuliyo@gmail.com>
2020-04-07 08:05:30 +0700

    dynamic callback

--- asynchronous/ajax-callback.js
+++ asynchronous/ajax-callback.js
@@ -2,7 +2,22 @@
 
 function buttonSearch(){
     const keyword = document.getElementById("keyword").value;
-    getProducts(keyword);
+    
+    getProducts(keyword, function (data){
+        clearProducts();
+        displayProducts(data);
+    }, function (){
+        getProductsError();
+    });
+
+    getProducts(keyword, function (data){
+        clearTableProducts();
+        displayTableProducts(data);
+    }, function (){
+        getProductsError();
+    });
+
+
     console.log("success click, ini di posisi synchronous")
 }
 
@@ -10,29 +25,37 @@ function getProductsUrl(keyword){
     return `https://www.blibli.com/backend/search/products?searchTerm=${keyword}`;
 }
 
-function getProducts(keyword){
+function getProducts(keyword, callbackSuccess, callbackError){
     // membuat instansiasi object ajax
     const ajax = new XMLHttpRequest();
 
     // onload adalah dipanggil ketika data yang diambil dari server di load ke ajax tsb
     ajax.onload = function(){
-        const data = JSON.parse(ajax.response);
-        clearProducts();
-        displayProducts(data);
+        // handel ajax
+        if(ajax.status === 200){
+            const data = JSON.parse(ajax.response);
+
+            // Dynamic Callback, function callback nya sebagai argument function getProducts()
+            callbackSuccess(data);
+        }else{
+            callbackError();
+        }
     }
 
     const url = getProductsUrl(keyword);
     ajax.open("GET", url, true);
     ajax.send();
 
-
-
-
 }
 
 function clearProducts(){
     const productUl = document.getElementById("products");
     productUl.textContent = "";
+}
+
+function getProductsError(){
+    console.log("error get products");
+    alert("Error get products!");
 }
 
 function displayProducts(data){
@@ -45,4 +68,23 @@ function displayProduct(product){
 
     const productUl = document.getElementById("products");
     productUl.appendChild(productLi);
+}
+
+function clearTableProducts(){
+    const productDiv = document.getElementById("table-products");
+    productDiv.textContent = "";
+}
+
+function displayTableProducts(data){
+    const table = document.createElement("table");
+    table.setAttribute("border", 1);
+
+    let index = 0;
+    data.data.products.forEach(product => {
+        table.insertRow(index).insertCell(0).innerText = product.name;
+        index++;
+    });
+
+    const tableProduct = document.getElementById("table-products");
+    tableProduct.appendChild(table);
 }
