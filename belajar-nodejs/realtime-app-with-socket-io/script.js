4b5c2a4f2b970a429723a6cffd983a52fd022e5f
eko muliyo <ekomuliyo@gmail.com>
2020-08-21 15:27:36 +0700

    initial project

---
+++ belajar-nodejs/realtime-app-with-socket-io/script.js
@@ -0,0 +1,37 @@
+const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('form-container');
+const messageInput = document.getElementById('message-input');
+
+const name = prompt('Masukan nama :')
+appendMessage('Anda Bergabung')
+
+socket.emit('new-user', name);
+
+// menangkap listener dari server
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`);
+    socket.emit('send-chat-message', message);
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = message;
+    messageContainer.append(messageElement);
+}
