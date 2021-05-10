d9e0482def801a55f9e947e3fada604f976cfe72
eko <ekomuliyo@gmail.com>
2021-05-10 13:15:30 +0700

    movie-catalogue

---
+++ web-expert-dicoding/movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    KEY: '58c189fe527bc494a863d6c84e93c18d',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME:  new Date().toISOString(),
+    DATABASE_NAME: 'movie-catalogue-database',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'movies',
+    WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com/'
+  };
+   
+  export default CONFIG;
